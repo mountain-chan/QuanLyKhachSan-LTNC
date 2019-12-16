e37b06e39ded4025158ec01ed49a3b0fb475c498
Dealer <nguyenkhanhsl1997@gmail.com>
2019-12-16 12:24:10 +0700

    update

---
+++ QLKS/src/java/model/LoaiKhachSan.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.io.Serializable;
+
+public class LoaiKhachSan implements Serializable {
+
+    private static final long serialVersionUID = 543213541L;
+    
+    int Id;
+    String Ten;
+    String MoTa;
+    String UrlHinhAnh;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String Ten) {
+        this.Ten = Ten;
+    }
+
+    public String getMoTa() {
+        return MoTa;
+    }
+
+    public void setMoTa(String MoTa) {
+        this.MoTa = MoTa;
+    }
+
+    public String getUrlHinhAnh() {
+        return UrlHinhAnh;
+    }
+
+    public void setUrlHinhAnh(String UrlHinhAnh) {
+        this.UrlHinhAnh = UrlHinhAnh;
+    }
+
+    public LoaiKhachSan() {
+
+    }
+
+    public LoaiKhachSan(int Id, String Ten, String MoTa, String UrlHinhAnh) {
+        this.Id = Id;
+        this.Ten = Ten;
+        this.MoTa = MoTa;
+        this.UrlHinhAnh = UrlHinhAnh;
+    }
+
+    public LoaiKhachSan(LoaiKhachSan tp) {
+        this.Id = tp.Id;
+        this.Ten = tp.Ten;
+        this.MoTa = tp.MoTa;
+        this.UrlHinhAnh = tp.UrlHinhAnh;
+    }
+
+    public void reload(int Id, String Ten, String MoTa, String UrlHinhAnh) {
+        this.Id = Id;
+        this.Ten = Ten;
+        this.MoTa = MoTa;
+        this.UrlHinhAnh = UrlHinhAnh;
+    }
+}
